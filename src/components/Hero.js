b6e4e946ae1ec56343c9e998715d083a2526e270
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-11-08 00:36:36 +0100

    deploying

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,7 +18,7 @@ const Hero=()=>{
                         <div className="px-[5rem]">
                             <h1 className="font-bold text-xl py-5">Cheerful Loving Couple Bakers Drinking Coffee</h1>
 
-                            <p className="text-base">It’s no secret that the digital industry is booming. From exciting startups to global brands, companies are reaching  out to  digital agencies, responding to the new possibilities available. However, the industry is fast becoming overcrowded, heaving  with agencies offering similar services — on the surface, at least. Producing creative, fresh projects is the key to standing out. Unique side projects are the best place to innovate, but balancing commercially and creatively lucrative work is tricky. So, this  article looks at …</p>
+                            <p className="text-base">It is no secret that the digital industry is booming. From exciting startups to global brands, companies are reaching  out to  digital agencies, responding to the new possibilities available. However, the industry is fast becoming overcrowded, heaving  with agencies offering similar services — on the surface, at least. Producing creative, fresh projects is the key to standing out. Unique side projects are the best place to innovate, but balancing commercially and creatively lucrative work is tricky. So, this  article looks at …</p>
 
                             <div className="py-5">
                                 <button className=" text-base border-orange-700 border px-4 py-2 rounded-full">Continue Reading</button>
@@ -38,7 +38,7 @@ const Hero=()=>{
                             <h1 className="font-bold text-xl py-5">Loft Office With Vintage Decor For Creative Working
                             </h1>
 
-                            <p className="text-base">It’s no secret that the digital industry is booming. From exciting startups to global brands, companies are reaching out to digital agencies, responding to the new possibilities available. However, the industry is fast becoming overcrowded, heaving with agencies offering similar services — on the surface, at least. Producing creative, fresh projects is the key to standing out. Unique side projects are the best place to innovate, but balancing commercially and creatively lucrative work is tricky. So, this article looks at …</p>
+                            <p className="text-base">It is no secret that the digital industry is booming. From exciting startups to global brands, companies are reaching out to digital agencies, responding to the new possibilities available. However, the industry is fast becoming overcrowded, heaving with agencies offering similar services — on the surface, at least. Producing creative, fresh projects is the key to standing out. Unique side projects are the best place to innovate, but balancing commercially and creatively lucrative work is tricky. So, this article looks at …</p>
 
                             <div className="py-5">
                                 <button className=" text-base border-orange-700 border px-4 py-2 rounded-full">Continue Reading</button>
@@ -58,7 +58,7 @@ const Hero=()=>{
                             <h1 className="font-bold text-xl py-5">Cosy Bright Office In Yellow And Grey Colors
                             </h1>
 
-                            <p className="text-base">It’s no secret that the digital industry is booming. From exciting startups to global brands, companies are reaching out to digital agencies, responding to the new possibilities available. However, the industry is fast becoming overcrowded, heaving with agencies offering similar services — on the surface, at least. Producing creative, fresh projects is the key to standing out. Unique side projects are the best place to innovate, but balancing commercially and creatively lucrative work is tricky. So, this article looks at …</p>
+                            <p className="text-base">It is no secret that the digital industry is booming. From exciting startups to global brands, companies are reaching out to digital agencies, responding to the new possibilities available. However, the industry is fast becoming overcrowded, heaving with agencies offering similar services — on the surface, at least. Producing creative, fresh projects is the key to standing out. Unique side projects are the best place to innovate, but balancing commercially and creatively lucrative work is tricky. So, this article looks at …</p>
 
                             <div className="py-5">
                                 <button className=" text-base border-orange-700 border px-4 py-2 rounded-full">Continue Reading</button>
